0049c386ae68a21238f4f5128bfb58752cbb9c38
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2022-05-29 14:02:15 -0300

    move duplicated code to setup method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -41,80 +41,88 @@ void main() {
     client = ClientSpy();
     sut = HttpAdapter(client);
     url = faker.internet.httpUrl();
+    when(client.post(any, headers: anyNamed("headers"), body: anyNamed("body")))
+        .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
   });
 
-  test(
-    'should call post without body',
-    () async {
-      when(client.post(any, headers: anyNamed("headers")))
-          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
-      await sut.request(
-        url: url,
-        method: 'post',
-      );
+  group('post', () {
+    PostExpectation mockRequest() => when(
+        client.post(any, headers: anyNamed("headers"), body: anyNamed("body")));
 
-      verify(
-        client.post(
-          any,
-          headers: anyNamed('headers'),
-        ),
-      );
-    },
-  );
-  test(
-    'should call post with correct values',
-    () async {
-      when(client.post(any,
-              headers: anyNamed("headers"), body: anyNamed("body")))
-          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
-      await sut.request(
-        url: url,
-        method: 'post',
-        body: {
-          "any_key": "any_value",
-        },
-      );
+    void mockResponse(
+      int statusCode, {
+      String body = '{"any_key":"any_value"}',
+    }) {
+      mockRequest().thenAnswer((_) async => Response(body, statusCode));
+    }
 
-      verify(
-        client.post(
-          Uri.parse(url),
-          headers: {
-            'content-type': 'application/json',
-            'accept': 'application/json'
+    setUp(() {
+      mockResponse(200);
+    });
+
+    test(
+      'should call post without body',
+      () async {
+        await sut.request(
+          url: url,
+          method: 'post',
+        );
+
+        verify(
+          client.post(
+            any,
+            headers: anyNamed('headers'),
+          ),
+        );
+      },
+    );
+    test(
+      'should call post with correct values',
+      () async {
+        await sut.request(
+          url: url,
+          method: 'post',
+          body: {
+            "any_key": "any_value",
           },
-          body: '{"any_key":"any_value"}',
-        ),
-      );
-    },
-  );
+        );
 
-  test(
-    'should return data if post returns 200',
-    () async {
-      when(client.post(any, headers: anyNamed("headers"))).thenAnswer(
-          (realInvocation) async => Response('{"any_key":"any_value"}', 200));
+        verify(
+          client.post(
+            Uri.parse(url),
+            headers: {
+              'content-type': 'application/json',
+              'accept': 'application/json'
+            },
+            body: '{"any_key":"any_value"}',
+          ),
+        );
+      },
+    );
 
-      final response = await sut.request(
-        url: url,
-        method: 'post',
-      );
+    test(
+      'should return data if post returns 200',
+      () async {
+        final response = await sut.request(
+          url: url,
+          method: 'post',
+        );
 
-      expect(response, {"any_key": "any_value"});
-    },
-  );
+        expect(response, {"any_key": "any_value"});
+      },
+    );
 
-  test(
-    'should return null if post returns 200 with no data',
-    () async {
-      when(client.post(any, headers: anyNamed("headers")))
-          .thenAnswer((realInvocation) async => Response('', 200));
+    test(
+      'should return null if post returns 200 with no data',
+      () async {
+        mockResponse(200, body: '');
+        final response = await sut.request(
+          url: url,
+          method: 'post',
+        );
 
-      final response = await sut.request(
-        url: url,
-        method: 'post',
-      );
-
-      expect(response, null);
-    },
-  );
+        expect(response, null);
+      },
+    );
+  });
 }
