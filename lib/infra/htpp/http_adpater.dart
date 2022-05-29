8cba261a6e193c3c627446d20b490654eb3570f1
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2022-05-29 14:12:43 -0300

    move HttpAdapter to its own file

---
+++ lib/infra/htpp/http_adpater.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_tdd_study/data/http/http.dart';
+import 'package:http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  @override
+  Future<Map> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    final response =
+        await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+
+    return response.statusCode == 200
+        ? response.body.isEmpty
+            ? null
+            : json.decode(response.body)
+        : null;
+  }
+}
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,38 +1,8 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_tdd_study/data/http/http_client.dart';
+import 'package:flutter_tdd_study/infra/htpp/htpp.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  @override
-  Future<Map> request({
-    @required String url,
-    @required String method,
-    Map body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json'
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response =
-        await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-
-    return response.statusCode == 200
-        ? response.body.isEmpty
-            ? null
-            : json.decode(response.body)
-        : null;
-  }
-}
 
 class ClientSpy extends Mock implements Client {}
 
