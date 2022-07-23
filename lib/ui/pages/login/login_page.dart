33249a82dadd0d5511a3a42847f91ced801b87b1
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2022-07-23 19:01:55 -0300

    ensure LoginPage validate when form changes

---
+++ lib/ui/pages/login/login_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tdd_study/ui/pages/login/login_presenter.dart';
+
+import '../../components/components.dart';
+
+class LoginPage extends StatelessWidget {
+  final LoginPresenter presenter;
+  const LoginPage({this.presenter, Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const LoginHeader(),
+            const HeadLine1(
+              text: 'Login',
+            ),
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      onChanged: presenter.validateEmail,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 8.0,
+                        bottom: 32,
+                      ),
+                      child: TextFormField(
+                        onChanged: presenter.validatePassword,
+                        decoration: const InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(Icons.lock),
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Text('Entrar'.toUpperCase()),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.person),
+                      label: const Text('Criar Conta'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -1,11 +1,18 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tdd_study/ui/pages/pages.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class LoginPresenterSpy extends Mock implements LoginPresenter {}
 
 void main() {
+  LoginPresenter presenter;
+
   Future<void> loadPage(WidgetTester tester) async {
-    const loginPage = MaterialApp(
-      home: LoginPage(),
+    presenter = LoginPresenterSpy();
+    final loginPage = MaterialApp(
+      home: LoginPage(presenter: presenter),
     );
     await tester.pumpWidget(loginPage);
   }
@@ -44,8 +51,18 @@ void main() {
     },
   );
 
-  testWidgets("Sould call validate with correct values",
-      (WidgetTester tester) async {
-    await loadPage(tester);
-  });
+  testWidgets(
+    "Sould call validate with correct values",
+    (WidgetTester tester) async {
+      await loadPage(tester);
+
+      final email = faker.internet.email();
+      await tester.enterText(find.bySemanticsLabel("Email"), email);
+      verify(presenter.validateEmail(email));
+
+      final password = faker.internet.password();
+      await tester.enterText(find.bySemanticsLabel("Senha"), password);
+      verify(presenter.validatePassword(password));
+    },
+  );
 }
