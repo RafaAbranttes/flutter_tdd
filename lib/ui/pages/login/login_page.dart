996eeb48eccc986efe55b8ac2fd58fafc1fbf823
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2022-08-10 14:52:36 -0300

    ensure LoginPage hide loading

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -36,6 +36,10 @@ class LoginPage extends StatelessWidget {
                     ],
                   ),
                 );
+              } else {
+                if (Navigator.canPop(context)) {
+                  Navigator.of(context).pop();
+                }
               }
             },
           );
--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -77,6 +77,7 @@ void main() {
 
       final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
       expect(button.onPressed, null);
+      expect(find.byType(CircularProgressIndicator), findsNothing);
     },
   );
 
@@ -228,6 +229,20 @@ void main() {
       await tester.pump();
 
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    "Should hide loading",
+    (WidgetTester tester) async {
+      await loadPage(tester);
+
+      isLoadingController.add(true);
+      await tester.pump();
+      isLoadingController.add(false);
+      await tester.pump();
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
     },
   );
 }
