90ead57987f9cbe533c92e272386b2e511957235
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2022-12-31 15:51:53 -0300

    refactor: change authentication usecase

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -2,7 +2,16 @@ import '../entities/entities.dart';
 
 abstract class Authenticaction {
   Future<AccountEntity> auth({
-    required String email,
-    required String password,
+    required AuthenticationParams authenticationParams,
+  });
+}
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({
+    required this.email,
+    required this.password,
   });
 }
