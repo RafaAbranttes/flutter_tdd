da820b6457d52ef26fa4f0765f2542f458c5f4b6
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2023-11-19 14:11:56 -0300

    ensure HttpAdapter returns data if post returns 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
 
 import 'package:faker/faker.dart';
+import 'package:flutter_tdd_study/data/http/http.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 
-class HttpAdapter {
+class HttpAdapter implements HttpClient {
   final Client? client;
 
   HttpAdapter({required this.client});
 
-  Future<void> request({
+  @override
+  Future<Map> request({
     required String url,
     required String method,
     Map? body,
@@ -19,11 +21,13 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client?.post(
+    final response = await client?.post(
       Uri.parse(url),
       headers: headers,
       body: jsonEncode(body),
     );
+
+    return jsonDecode(response?.body ?? "") as Map;
   }
 }
 
@@ -39,26 +43,74 @@ void main() {
     sut = HttpAdapter(client: client ?? ClientSpy());
     url = faker.internet.httpUrl();
   });
-  group('post', () {
-    test('Should call post with correct values', () async {
-      await sut?.request(
-        url: url ?? '',
-        method: 'post',
-        body: {
-          'any_key': 'any_value',
+  group(
+    'post',
+    () {
+      test(
+        'Should call post with correct values',
+        () async {
+          when(
+            client?.post(
+              Uri.parse(url ?? ''),
+              body: anyNamed('body'),
+              headers: anyNamed('headers'),
+            ),
+          ).thenAnswer(
+            (_) async => Response(
+              '{"any_key":"any_value"}',
+              200,
+            ),
+          );
+
+          await sut?.request(
+            url: url ?? '',
+            method: 'post',
+            body: {
+              'any_key': 'any_value',
+            },
+          );
+
+          verify(
+            client?.post(
+              Uri.parse(url ?? ''),
+              headers: {
+                'content-type': 'application/json',
+                'accept': 'application/json',
+              },
+              body: '{"any_key":"any_value"}',
+            ),
+          );
         },
       );
 
-      verify(
-        client?.post(
-          Uri.parse(url ?? ''),
-          headers: {
-            'content-type': 'application/json',
-            'accept': 'application/json',
-          },
-          body: '{"any_key":"any_value"}',
-        ),
+      test(
+        'Should return data if post returns 200',
+        () async {
+          when(
+            client?.post(
+              Uri.parse(url ?? ''),
+              headers: anyNamed('headers'),
+            ),
+          ).thenAnswer(
+            (_) async => Response(
+              '{"any_key":"any_value"}',
+              200,
+            ),
+          );
+
+          final response = await sut?.request(
+            url: url ?? '',
+            method: 'post',
+          );
+
+          expect(
+            response,
+            {
+              'any_key': 'any_value',
+            },
+          );
+        },
       );
-    });
-  });
+    },
+  );
 }
